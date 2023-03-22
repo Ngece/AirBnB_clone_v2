3fe635b95ffb7b57e11474f17deb15b6c63f5c50
Ngece <ngecemlamli@gmail.com>
2023-03-22 15:30:25 -0700

    committing

--- models/base_model.py
+++ models/base_model.py
@@ -14,6 +14,25 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+                if 'id' not in kwargs:
+                    self.id = str(uuid.uuid4())
+                if 'created_at' not in kwargs:
+                    self.created_at = datetime.now()
+
+                if 'created_at' in kwargs and 'updated_at' not in kwargs:
+                    self.updated_at = self.created_at
+                else:
+                    self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+"""
         if not kwargs:
         
             self.id = str(uuid.uuid4())
@@ -28,6 +47,7 @@ class BaseModel:
             del kwargs['__class__']
             self.__dict__.update(kwargs)
 
+"""
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
